d219bcb48f939b88835d2ce83a9096e693a785c5
bkenobi <bkenobi69@yahoo.com>
2015-12-26 17:22:26 -0800

    Create DecodeOOK.h

---
+++ DecodeOOK.h
@@ -0,0 +1,109 @@
+#include <Arduino.h>
+
+class DecodeOOK {
+protected:
+    byte total_bits, bits, flip, state, pos, data[25];
+    
+    virtual char decode (word width) =0;
+
+public:
+
+    enum { UNKNOWN, T0, T1, T2, T3, OK, DONE };
+
+    DecodeOOK () { resetDecoder(); }
+
+    bool nextPulse (word width) {
+        if (state != DONE)
+            switch (decode(width)) {
+                case -1: resetDecoder(); break;
+                case 1:  done(); break;
+            }
+        return isDone();
+    }
+
+    bool isDone () const { return state == DONE; }
+
+    const byte* getData (byte& count) const {
+        count = pos;
+        return data;
+    }
+
+    void resetDecoder () {
+        total_bits = bits = pos = flip = 0;
+        state = UNKNOWN;
+        //Serial.println("DecodeOOK.resetDecoder");
+        //Serial.println();
+    }
+
+    // add one bit to the packet data buffer
+
+    virtual void gotBit (char value) {
+        total_bits++;
+        byte *ptr = data + pos;
+        *ptr = (*ptr >> 1) | (value << 7);
+
+        if (++bits >= 8) {
+            bits = 0;
+            if (++pos >= sizeof data) {
+                resetDecoder();
+                return;
+            }
+        }
+    }
+
+    // store a bit using Manchester encoding
+    void manchester (char value) {
+        flip ^= value; // manchester code, long pulse flips the bit
+        gotBit(flip);
+    }
+
+    // move bits to the front so that all the bits are aligned to the end
+    void alignTail (byte max =0) {
+        // align bits
+        if (bits != 0) {
+            data[pos] >>= 8 - bits;
+            for (byte i = 0; i < pos; ++i)
+                data[i] = (data[i] >> bits) | (data[i+1] << (8 - bits));
+            bits = 0;
+        }
+        // optionally shift bytes down if there are too many of 'em
+        if (max > 0 && pos > max) {
+            byte n = pos - max;
+            pos = max;
+            for (byte i = 0; i < pos; ++i)
+                data[i] = data[i+n];
+        }
+    }
+
+    void reverseBits () {
+        for (byte i = 0; i < pos; ++i) {
+            byte b = data[i];
+            for (byte j = 0; j < 8; ++j) {
+                data[i] = (data[i] << 1) | (b & 1);
+                b >>= 1;
+            }
+        }
+    }
+
+    void reverseNibbles () {
+        for (byte i = 0; i < pos; ++i)
+            data[i] = (data[i] << 4) | (data[i] >> 4);
+    }
+
+    void done () {
+        while (bits)
+            gotBit(0); // padding
+        state = DONE;
+    }
+
+    void PrintRaw (void) {
+      char packet[100];
+      
+      sprintf(packet,"");
+      for (byte i = 0; i < pos; ++i) {
+        sprintf(packet,"%s%02X",packet,data[i]);
+      }
+      sprintf(packet,"  ");
+      Serial.print(packet);
+    }
+};
